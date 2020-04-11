30636b1dab81aa8753464d43fe7733f82d0caacb
Charles Buxton <chazbux@gmail.com>
2020-04-11 14:43:55 -0600

    auth working

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
+import { initMiddleware, } from 'devise-axios';
 import { AuthProvider, } from "./providers/AuthProvider";
 import 'semantic-ui-css/semantic.min.css';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
