92227505f94fb3323ffa4dd4183ad1e45f1a284e
Scott Key <scottdkey@gmail.com>
2020-04-11 14:50:32 -0600

    view facilities

--- client/src/components/testing_facilities.jsx
+++ client/src/components/testing_facilities.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import { Component } from 'react'
-import FacilitiesForm from './testing_facilitiesForm'
-import {Button, Modal} from 'semantic-ui-react'
-
+import React from "react";
+import { Component } from "react";
+import FacilitiesForm from "./testing_facilitiesForm";
+import { Button, Modal } from "semantic-ui-react";
+import axios from "axios";
 
 export default class TestingFacilities extends Component {
-state = {
-  facilies: [],
-  newForm: false,
-}
+  state = {
+    facilies: [],
+    newForm: false
+  };
 
-toggleForm =()=>{
-this.setState({
-  newForm: !this.state.newForm
-})
-}
+  componentDidMount(){
+    this.getFacilities()
+  };
 
-render(){
-  return (
-    <>
-      <Button onClick={this.toggleForm}>New </Button>
-      <Modal open={this.state.newForm} close={this.toggleForm}>
-        <FacilitiesForm toggleForm={this.toggleForm} />
-      </Modal>
-    </>
-  );
-}
+  getFacilities = async() => {
+        const res = await axios.get(`/api/testing_facilities/`);
+        this.setState({
+          facilies: res.data
+        });
+  }
 
+  toggleForm = () => {
+    this.setState({
+      newForm: !this.state.newForm
+    });
+  };
+
+  renderAllFacilities = () => (
+    this.state.facilies.map(facility => {
+      return (
+        <>
+          <div>{facility.name}</div>
+        </>
+      );
+    })
+  )
+
+  render() {
+    return (
+      <>
+        <Button onClick={this.toggleForm}>New </Button>
+        <Modal open={this.state.newForm} close={this.toggleForm}>
+          <FacilitiesForm toggleForm={this.toggleForm} />
+        </Modal>
+        {this.renderAllFacilities()}
+      </>
+    );
+  }
 }
