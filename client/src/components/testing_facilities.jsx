9d675646b94173304be01f60404aa05f78ee2538
Scott Key <scottdkey@gmail.com>
2020-04-11 13:49:46 -0600

    Components added to flow

--- client/src/components/testing_facilities.jsx
+++ client/src/components/testing_facilities.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Component } from 'react'
+import FacilitiesForm from './testing_facilitiesForm'
 
 
 export default class TestingFacilities extends Component {
-state = {}
+state = {
+  facilies: []
+}
 
 render(){
   return(
     <>
       <p>on Testing Facilities page</p>
+      <FacilitiesForm />
     </>
   )
 }
---
+++ client/src/components/testing_facilitiesForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Component } from 'react'
+
+export default class FacilitiesForm extends Component{
+state ={} 
+
+render(){
+  return(
+    <>
+    <p>Facilities Form</p>
+    </>
+  )
+}
+}
