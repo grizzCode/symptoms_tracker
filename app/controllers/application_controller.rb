807c896df08f8ca3138e550fde9ee64959b10da1
Charles Buxton <chazbux@gmail.com>
2020-04-11 14:06:43 -0600

    auth tweaks
    Co-authored-by: Scott Key <scottdkey@gmail.com>
    Co-authored-by: jgreene21 <julie_greene22@yahoo.com>
    Co-authored-by: Eder Espinoza <yahired3@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        protect_from_forgery with :null_session
         before_action :configure_permitted_parameters, if: :devise_controller?
       
         protected
