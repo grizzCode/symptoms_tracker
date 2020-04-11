70b668a7df2bc16f3de189536f2e04462cf14e9a
Scott Key <scottdkey@gmail.com>
2020-04-11 10:46:02 -0600

    app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import FetchUser from './components/FetchUser'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import NoMatch from './components/NoMatch'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {Container} from 'semantic-ui-react'
+
+
 
 function App() {
   return (
